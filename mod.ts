70c319452d4926bf6548bc8fda60a011e313a1ff
Joshua Byrd <phocks@gmail.com>
2024-10-16 11:16:18 +1000

    Add docs?

--- mod.ts
+++ mod.ts
@@ -1,3 +1,16 @@
+/**
+ * @module
+ *
+ * This module return the nth number in the Fibonacci sequence.
+ *
+ * @example
+ * ```ts
+ * import { fib } from "./mod.ts";
+ *
+ * console.log(fib(3)); // 2
+ * console.log(fib(5)); // 5
+ * ```
+ */
 const fib = (n: number): number => {
   if (n <= 1) {
     return n;
